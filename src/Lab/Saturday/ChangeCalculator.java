ae5b67dcf4aa7ad0f6c68b9334b10ff4f9c8599f
Santa Dunaiska <61906114+SantaDunaiska@users.noreply.github.com>
2020-03-09 19:08:36 +0200

    Update ChangeCalculator.java

--- src/Lab/Saturday/ChangeCalculator.java
+++ src/Lab/Saturday/ChangeCalculator.java
@@ -1,5 +1,5 @@
 package Lab.Saturday;
-import java.util.*;
+
 
 /**
 Calculates the change owned to a customer.
@@ -19,24 +19,42 @@ Write, compile and test whether the application provides the right change.
 Save the class as ChangeCalculator.java
 */
 
-
-public class ChangeCalculator {
-
-
-	public static void main(String[] args) {
-		
-		int price,received,change;
-		int pay50, pay20, pay10, pay5, pay2, pay1;
-		
-		Scanner sc= new Scanner(System.in);
-		System.out.println("Please enter the price of the item :");
-		price = sc.nextInt();
-		System.out.println("Please enter the received amount: ");
-		received = sc.nextInt();
-		
-		
-		
-	
-	}
-
+import java.util.Scanner;
+public class ChangeCalculator {	public static void main(String[] args){
+		// declare variables
+		int price, received;		int change;
+		int pay50, pay20, pay10, pay5, pay2, pay1;		// input
+		// declare a variable of type Scanner
+		Scanner input;
+		// create an object of type Scanner to accept the user's input from the keyboard
+		input = new Scanner(System.in);		System.out.println("enter total price:");
+		price = input.nextInt(); // read the total price provided by the user via keyboard
+		// above we saved in the variable price the total price entered by the user 		System.out.println("enter amount received:");
+		// save the money given by the user
+		received = input.nextInt();		// processing + output
+		// calculate the change owed to the customer
+		change = received - price;
+		System.out.println("give to the customer " + change + "EUR");		// calculate the number of 50 EUR notes to be given
+		pay50 = change / 50;
+		System.out.println("50EUR notes: " + pay50);		// update the current change
+		// we must update the remaining change to be given to the customer because we have already established the number of 50 EUR note(s) to be given
+		// if we do not update the change we would give more money back to the customer than required
+		// we can save/store the remaining received to be paid in the same variable change because we do not use further the value currently stored in the variable change (therefore we overwrite the existing value) as that value contains also the amount received of 50 EUR note(s) already accounted for in the variable pay50
+		change = change - 50 * pay50;		// calculate the number of EUR 20 notes to be given
+		pay20 = change/20;
+		System.out.println("20EUR notes: " + pay20);		// update the current change
+		change = change - 20 * pay20;		// TODO finish the implementation of this problem 		
+			pay10 = change/10;
+		System.out.println("10EUR notes: " + pay10);		// update the current change
+			change = change - 10 * pay10;	
+	pay5 = change/5;
+		System.out.println("5EUR notes: " + pay5);		// update the current change
+			change = change - 5 * pay5;
+	pay2 = change/2;
+		System.out.println("2EUR notes: " + pay2);		// update the current change
+			change = change - 2 * pay2;
+	pay1 = change/1;
+		System.out.println("1EUR notes: " + pay1);		// update the current change
+			change = change - 1 * pay1;
+}
 }
